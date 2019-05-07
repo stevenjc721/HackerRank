eb4aa0c5f1b6707c92a5429efda50e1cb4891a75
stevenjc741@outlook.com <stevenjc741@outlook.com>
2019-05-06 20:11:34 -0400

    MinimumSwap

---
+++ HackerRank/Arrays/MinimumSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+/* 
+ * <summary>
+ * You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. 
+ * You need to find the minimum number of swaps required to sort the array in ascending order.
+ *  ex: [7,1,3,2,4,5,6]
+ *      [2,1,3,7,4,5,6] swap(0,3)
+ *      [1,2,3,7,4,5,6] swap(0,1)
+ *      [1,2,3,4,7,5,6] swap(3,4)
+ *      [1,2,3,4,5,7,6] swap(4,5)
+ *      [1,2,3,4,5,6,7] swap(5,6)
+ * </summary>
+ */
+namespace HackerRank.Arrays {
+    public class MinimumSwap
+    {
+            // Complete the minimumSwaps function below.
+            public int minimumSwaps(int[] arr)
+            {
+
+            return 0; 
+            }
+        
+    }
+}
